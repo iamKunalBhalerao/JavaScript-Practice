d16d0a1934eea847e88cb99bdf9845b9eb99612e
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-09-02 19:10:35 +0530

    left side color done

--- Practice 1/index.js
+++ Practice 1/index.js
@@ -1,6 +1,32 @@
 var rect = document.querySelector("#center");
 
 rect.addEventListener("mousemove", (details) => {
-  var cursorLocation = rect.getBoundingClientRect();
-  console.log(details.clientX - cursorLocation.left);
+  var rectLocation = rect.getBoundingClientRect();
+  var insideRectVal = details.clientX - rectLocation.left;
+
+  if (insideRectVal < rectLocation.width / 2) {
+    var redcolor = gsap.utils.mapRange(
+      0,
+      rectLocation.width / 2,
+      255,
+      0,
+      insideRectVal
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(${redcolor}, 0, 0)`,
+      ease: Power4,
+    });
+  } else {
+    var bluecolor = gsap.utils.mapRange(
+      0,
+      rectLocation.width / 2,
+      255,
+      0,
+      insideRectVal
+    );
+    gsap.to(rect, {
+      backgroundColor: `rgb(0, ${bluecolor}, 0)`,
+      ease: Power4,
+    });
+  }
 });
