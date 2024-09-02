015a9dcb72f2aa5c2accfbc2f52328dfa585e8ce
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-09-02 13:26:58 +0530

    getting the value of cursor from left

--- Practice 1/index.js
+++ Practice 1/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World!");
+var rect = document.querySelector("#center");
+
+rect.addEventListener("mousemove", (details) => {
+  var cursorLocation = rect.getBoundingClientRect();
+  console.log(details.clientX - cursorLocation.left);
+});
