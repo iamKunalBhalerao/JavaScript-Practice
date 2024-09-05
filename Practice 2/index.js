725d4486270ed92e870f1b7d95fd714a80281e76
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-09-05 07:53:07 +0530

    JavaScript Practice 2 Done

--- Practice 2/index.js
+++ Practice 2/index.js
@@ -1,1 +1,17 @@
-console.log("This is JavaScript Practice 2");
+window.addEventListener("mousemove", (details) => {
+  var rect = document.querySelector("#rect");
+
+  let xval = gsap.utils.mapRange(
+    0,
+    window.innerWidth,
+    200 + rect.getBoundingClientRect().width / 2,
+    window.innerWidth - (200 + rect.getBoundingClientRect().width / 2),
+    details.clientX
+  );
+  console.log(details);
+
+  gsap.to("#rect", {
+    left: xval + "px",
+    ease: Power3,
+  });
+});
